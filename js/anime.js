521e014891ca6aa63db2abe34dae7d9133a016f1
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2021-10-28 22:56:24 +0200

    Initial commit

---
+++ js/anime.js
@@ -0,0 +1,38 @@
+let imgbx = document.querySelectorAll('.imgbx')
+imgbx.forEach(popup => popup.addEventListener('click', () => 
+{
+    popup.classList.toggle('active')
+}))
+
+
+
+
+
+function toggle(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup')
+    popup.classList.toggle('active');
+    
+}
+
+
+function togglee(){
+    var blurr = document.getElementById('blur')
+    blurr.classList.toggle('active');
+    var popupp = document.getElementById('popupp')
+    popupp.classList.toggle('active');
+}
+
+document.addEventListener("mousemove", function(e){
+    const bg = document.querySelector('.bg');
+    const yu = document.querySelector('.yu');
+    const content = document.querySelector('.content');
+
+    bg.style.width = 100 + e.pageX/100 + '%';
+    bg.style.height = 100 + e.pageX/100 + '%';
+
+    yu.style.right = 100 + e.pageX/2 + 'px';
+
+    content.style.left = 100 + e.pageX/3 + 'px';
+})
