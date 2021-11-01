642b5f9dac09dffd6400b08c327458656eb5bab4
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2021-11-01 01:47:00 +0100

    css and html update
    text add and css update

--- include.php
+++ include.php
@@ -2,9 +2,8 @@
             <a href="javascript: void(0)" class="bouton" onclick="closeMenu()">&times;</a>
             <div class="list-plongeante">
                 <a href="index.php">Home</a>
-                <a href="contact.php">contact</a>
-                <a href="project.php">Baam and Rachel</a>
-                <a href="skils.php">screen saver</a>
+                <a href="project.php">My project</a>
+                <a href="skils.php">skils</a>
             </div>
         </div>
         <!-- icone menu burger -->
--- skils.php
+++ skils.php
@@ -29,6 +29,14 @@ include("include.php");
             <p>js (still learning but im ok)</p>
             <p>python (learning)</p>
          </div>
+         <div class="grid7">
+             <p>in css i have some skils like ussing grid flex and other thing <br> i can do preaty thing you almost ask my Anything and if i dont know how to do it <br> i 
+            will lear how to do it in js i can also do some great stuff you just need to tell my what you want and i will find it and make it like you want <br> html is like tapping text so i think im pretty go whit it its a basics who you need whatever the case</p>
+         </div>
+         <div class="grid8">
+             <p>Working in group cause no probleme for my at the opposit i do great i have a lot of idea and i don't want to feel useless <br>so i always do something for the group and try to make a good vibe in the group because <br>work dose not always mean to be serious and boring</p>
+         </div>
+
 
          <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
          <script type="text/javascript" src="js/anime.js"></script>
